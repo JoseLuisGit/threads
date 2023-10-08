6ff526272e8daaa90ff528c63e9f5adec1576e42
Jose Luis Quispe <luis.quispe@a2odev.com>
2023-10-08 13:45:08 -0400

    init project and create thread

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿static void countNumbers(){
+    Console.WriteLine("Starting");
+    for(int i = 1; i <= 10; i++){
+        Console.WriteLine(i);
+    }
+}
+
+Thread thread = new Thread(countNumbers);
+thread.Start();
+
+countNumbers();
